a3eb355d685fa84195479c027853921f32a70798
MichaelPret <ST10477647@vcconnect.edu.za>
2025-03-07 10:52:39 +0200

    Added The File

---
+++ Classes/src/main/java/com/mycompany/classes/Classes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classes;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Classes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
