74853e4911f38ff4eeca6e33d9d98a0f714a6e95
MichaelPret <ST10477647@vcconnect.edu.za>
2025-03-17 08:28:33 +0200

    added
    added

--- ClassesOne/src/main/java/com/mycompany/classesone/ClassesOne.java
+++ ClassesOne/src/main/java/com/mycompany/classesone/ClassesOne.java
@@ -11,28 +11,28 @@ import javax.swing.JOptionPane;
  * @author lab_services_student
  */
 public class ClassesOne {
-    private static Students s = new Students();
+    private static Students s = new Students(); //object of our studnet class
 
     public static void main(String[] args) {
-         String strName = JOptionPane.showInputDialog(null, "please enter student name");
-          String strCourse = JOptionPane.showInputDialog(null, "plesae enter student coure");
-                  double dblAvg =Double.parseDouble (JOptionPane.showInputDialog(null, "Please enter student course"));
+         String strName = JOptionPane.showInputDialog(null, "please enter student name");   //getting input using joption.pan by using pop up dialog
+          String strCourse = JOptionPane.showInputDialog(null, "plesae enter student course");
+                  double dblAvg =Double.parseDouble (JOptionPane.showInputDialog(null, "Please enter Average"));
                   
                   
                   
-                  s.setName (strName);
+                  s.setName (strName);   //using variables. accessing the methos. variables sending messages to the class
                   s.setCourse (strCourse);
                   s.setAverage (dblAvg);
-                  print();
+                  print();  // method call
     }
                        public static void print()
                        {
-                           String strDisplay =  "the updated student average results are\n " 
-                                                + "student: " + s.getName () + "\n" 
+                           String strDisplay =  "the updated student average results are\n "  //returns nothing  
+                                                + "student: " + s.getName () + "\n"         
                                                 + "Course : " + s.getCourse() + "\n"
                                                 +"Updated avergae" + s.getAverage() +"%";
                            
-                           JOptionPane.showMessageDialog(null, strDisplay);
+                           JOptionPane.showMessageDialog(null, strDisplay);//displaying the message
                   
     }
 }
